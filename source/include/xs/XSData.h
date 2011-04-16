9208e57d727e1b2aef16377a2d62f779858acb38
Jean-David Gadina <macmade@eosgarden.com>
2011-04-16 05:28:44 +0200

    Added inline documentation.

--- source/include/xs/XSData.h
+++ source/include/xs/XSData.h
@@ -56,22 +56,65 @@ typedef struct XSData * XSData;
  */
 XSStatic XSObject XSData_Alloc( void );
 
+/*!
+ * @function    XSData_Init
+ * @abstract    Creates a data object
+ * @param       xsThis  The data object
+ * @result      The data object
+ */
 XSObject XSData_Init( XSObject xsThis );
 
+/*!
+ * @function    XSData_InitWithCapacity
+ * @abstract    Creates a data object with an initial capacity
+ * @param       xsThis      The data object
+ * @param       capacity    The initial capacity
+ * @result      The data object
+ */
 XSData XSData_InitWithCapacity( XSData xsThis, XSUInteger capacity );
 
+/*!
+ * @function    XSData_InitWithBytes
+ * @abstract    Creates a data object with bytes
+ * @param       xsThis      The data object
+ * @param       bytes       The data to copy
+ * @param       capacity    The length of the data
+ * @result      The data object
+ */
 XSData XSData_InitWithBytes( XSData xsThis, UInt8 * bytes, XSUInteger length );
 
+/*!
+ * @function    XSData_GetLength
+ * @abstract    Gets the data length
+ * @result      The data length
+ */
 XSUInteger XSData_GetLength( XSData xsThis );
 
+/*!
+ * @function    XSData_GetBytes
+ * @abstract    Get bytes from the data object
+ * @param       xsThis      The data object
+ * @param       range       The range of the data to get
+ * @result      The data bytes
+ */
 XSAutoreleased UInt8 * XSData_GetBytes( XSData xsThis, XSRange range );
 
-void XSData_SetBytes( XSData xsThis, UInt8 * bytes, XSRange range );
-
+/*!
+ * @function    XSData_AppendBytes
+ * @abstract    Appends bytes to the data object
+ * @param       xsThis      The data object
+ * @param       bytes       The bytes to append
+ * @param       length      The length of the data to append
+ * @result      The data bytes
+ */
 void XSData_AppendBytes( XSData xsThis, UInt8 * bytes, XSUInteger length );
 
-void XSData_DeleteBytes( XSData xsThis, XSRange range );
-
+/*!
+ * @function    XSData_GetBytesPointer
+ * @abstract    Gets a pointer to the bytes
+ * @param       xsThis      The data object
+ * @result      A pointer to the bytes data
+ */
 UInt8 * XSData_GetBytesPointer( XSData xsThis );
 
 XS_EXTERN_C_END
