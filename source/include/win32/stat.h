30af86fbbce184ac63cccc79e95480be483cb1ea
Jean-David Gadina <macmade@eosgarden.com>
2011-04-16 14:13:33 +0200

    The stat struct is defined on Windows.

--- source/include/win32/stat.h
+++ source/include/win32/stat.h
@@ -76,23 +76,6 @@ extern "C" {
 #define S_ISUID     0
 #define S_ISGID     0
 
-struct stat
-{
-    dev_t     st_dev;       /* ID of device containing file */
-    ino_t     st_ino;       /* Inode number */
-    mode_t    st_mode;      /* Protection */
-    nlink_t   st_nlink;     /* Number of hard links */
-    uid_t     st_uid;       /* User ID of owner */
-    gid_t     st_gid;       /* Group ID of owner */
-    dev_t     st_rdev;      /* Device ID (if special file) */
-    off_t     st_size;      /* Total size, in bytes */
-    blksize_t st_blksize;   /* Blocksize for filesystem I/O */
-    blkcnt_t  st_blocks;    /* Number of blocks allocated */
-    time_t    st_atime;     /* Time of last access */
-    time_t    st_mtime;     /* Time of last modification */
-    time_t    st_ctime;     /* Time of last status change */
-};
-
 int stat( const char * path, struct stat * buf );
 
 #ifdef __cplusplus
