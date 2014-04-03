86d5d3271a9db3bbb79d2e85b5f2e6b014764bee
Jean-David Gadina <macmade@xs-labs.com>
2014-04-04 01:15:48 +0200

    Added global initialisation.

--- XSFoundation/source/XS/Functions/XSProcess/__XSProcess.c
+++ XSFoundation/source/XS/Functions/XSProcess/__XSProcess.c
@@ -71,5 +71,5 @@
 #include <XS/XS.h>
 #include <XS/__private/Functions/XSProcess.h>
 
-char __XSProcess_ProcessName[ __XS_PROCESS_NAME_MAX ];
+char __XSProcess_ProcessName[ __XS_PROCESS_NAME_MAX ] = { 0 };
 volatile XSInteger __XSProcess_ProcessNameStatus = XSInitStatusNotInited;
