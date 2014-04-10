f5f48a843a77141d6b622604c981649cd0095398
Jean-David Gadina <macmade@xs-labs.com>
2014-04-10 19:52:52 +0200

    XSApplication is now a singleton.

--- XSFoundation/source/XS/Classes/XSApplication/__XSApplication.c
+++ XSFoundation/source/XS/Classes/XSApplication/__XSApplication.c
@@ -75,7 +75,7 @@ XSClassID   __XSApplication_ClassID = 0;
 XSClassInfo __XSApplication_Class   =
 {
     "XSApplication",
-    XSClassTypeNormal,
+    XSClassTypeSingleton,
     sizeof( struct __XSApplication ),
     ( XSClassCallbackConstructor )__XSApplication_Constructor,
     ( XSClassCallbackDestructor  )__XSApplication_Destructor,
