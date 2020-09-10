c8b117a7bb9a3851f683df8c73da6dc328d4bf4a
macmade <macmade@xs-labs.com>
2020-09-10 13:18:42 +0200

    XSCopyWithInfos

---
+++ XSFoundation/source/XS/Functions/Memory/XSCopyWithInfos.c
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2020 Jean-David Gadina - www.xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/*!
+ * @file        XSCopyWithInfos.c
+ * @copyright   (c) 2020 - Jean-David Gadina - www.xs-labs.com
+ * @author      Jean-David Gadina - www.xs-labs.com
+ * @abstract    Definition for XSCopyWithInfos
+ */
+
+#include <XS/XS.h>
+#include <XS/Private/Functions/Memory.h>
+#include <XS/Private/Functions/Runtime.h>
+#include <string.h>
+
+void * XSCopyWithInfos( const void * memory, const char * file, int line, const char * func )
+{
+    XSMemoryObject *    object;
+    void *              data;
+    void *              copiedData;
+    XSClassCallbackCopy copy;
+
+    if( memory == NULL )
+    {
+        return NULL;
+    }
+
+    object = XSGetMemoryObject( memory );
+
+    // TODO
+    //__XSDebugger_CheckObjectIntegrity( object );
+
+    data = XSAllocWithInfos( object->size, object->classID, file, line, func );
+
+    if( XSRuntimeIsRegisteredClass( object->classID ) == false )
+    {
+        memcpy( data, memory, object->size );
+
+        return data;
+    }
+
+    copy = XSRuntimeGetCopyCallback( object->classID );
+
+    if( copy != NULL )
+    {
+        copiedData = XS_UNSAFE_POINTER_CAST( void *, copy( memory, data ) );
+
+        if( copiedData == NULL )
+        {
+            XSRelease( data );
+
+            data = NULL;
+        }
+        else
+        {
+            data = copiedData;
+        }
+    }
+    else
+    {
+        memcpy( data, memory, object->size );
+    }
+
+    return data;
+}
