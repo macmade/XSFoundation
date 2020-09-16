e4450f1f348217a0c5d77e633e77e2dc6605ec1c
macmade <macmade@xs-labs.com>
2020-09-16 21:53:11 +0200

    Unit-tests

---
+++ Unit-Tests/Atomic/XSAtomicDecrement32.c
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2020 Jean-David Gadina - www.xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/*!
+ * @file        XSAtomicDecrement32.c
+ * @copyright   (c) 2020 - Jean-David Gadina - www.xs-labs.com
+ * @author      Jean-David Gadina - www.xs-labs.com
+ */
+
+#include <XSCTest/XSCTest.h>
+#include <XS/XS.h>
+
+Test( Atomic, XSAtomicDecrement32 )
+{
+    int32_t i = 5;
+
+    for( int32_t j = 0; j < 10; j++ )
+    {
+        AssertEqual( i, 5 - j );
+        XSAtomicDecrement32( &i );
+        AssertEqual( i, ( 5 - j ) - 1 );
+    }
+}
+
+Test( Atomic, XSAtomicDecrement32_Min )
+{
+    int32_t i = INT32_MIN;
+
+    for( int32_t j = 0; j < 10; j++ )
+    {
+        XSAtomicDecrement32( &i );
+        AssertEqual( i, INT32_MAX - j );
+    }
+}
