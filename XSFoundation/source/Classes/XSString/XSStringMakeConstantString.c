c691f3d20025e56ba5046466ae95cb7acadb2781
macmade <macmade@xs-labs.com>
2020-09-30 00:16:23 +0200

    Windows fixes

--- XSFoundation/source/Classes/XSString/XSStringMakeConstantString.c
+++ XSFoundation/source/Classes/XSString/XSStringMakeConstantString.c
@@ -93,7 +93,7 @@ add:
         }
     }
 
-    XSStringConstantStrings = realloc( ( void * )XSStringConstantStrings, 2 * XSStringConstantStringsCapacity * sizeof( struct XSString * ) );
+    XSStringConstantStrings = ( const struct XSString ** )realloc( ( void * )XSStringConstantStrings, 2 * XSStringConstantStringsCapacity * sizeof( struct XSString * ) );
 
     if( XSStringConstantStrings == NULL )
     {
